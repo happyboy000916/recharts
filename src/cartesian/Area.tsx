happyboy000916/recharts

commit 7354b54c2cf6bf3639a162de8bd2cc3365d40eaa

Fix Area's `baseValue` prop (#3013)

src/cartesian/Area.tsx
@@ -118,8 +118,9 @@ export class Area extends PureComponent<Props, State> {
     animationEasing: 'ease',
   };
 
-  static getBaseValue = (props: Props, xAxis: Props['xAxis'], yAxis: Props['yAxis']): number => {
-    const { layout, baseValue } = props;
+  static getBaseValue = (props: Props, item: Area, xAxis: Props['xAxis'], yAxis: Props['yAxis']): number => {
+    const { layout } = props;
+    const { baseValue } = item.props;
 
     if (isNumber(baseValue) && typeof baseValue === 'number') {
       return baseValue;
@@ -154,6 +155,7 @@ export class Area extends PureComponent<Props, State> {
 
   static getComposedData = ({
     props,
+    item,
     xAxis,
     yAxis,
     xAxisTicks,
@@ -180,7 +182,7 @@ export class Area extends PureComponent<Props, State> {
   }) => {
     const { layout } = props;
     const hasStack = stackedData && stackedData.length;
-    const baseValue = Area.getBaseValue(props, xAxis, yAxis);
+    const baseValue = Area.getBaseValue(props, item, xAxis, yAxis);
     let isRange = false;
 
     const points = displayedData.map((entry, index) => {

commit ab7ec1de89706ffac3c776ddc0ca695900eb8b7b

AreaChart: Respect baseValue of AreaChart, but allow override from Area (#3140)
Fix a bug, introduced in https://github.com/recharts/recharts/pull/3013,
which neglects to respect the AreaChart baseValue and thereby breaks
existing charts.

## Related Issue
https://github.com/recharts/recharts/issues/3051

## How Has This Been Tested?
I tested the change locally by running the demo project, verifying that
the baseValue of the AreaChart is effective by itself, and that
overriding it per Area component is possible.

## Screenshots (if appropriate):
<img width="1319" alt="Screenshot 2023-01-04 at 15 20 14"
src="https://user-images.githubusercontent.com/27995606/210575218-491c0a7e-1e80-4d98-b06b-2bf706a5694c.png">
<img width="1319" alt="Screenshot 2023-01-04 at 15 20 04"
src="https://user-images.githubusercontent.com/27995606/210575226-6e029126-a7c1-4685-b196-2a9333af91d2.png">
<img width="1319" alt="Screenshot 2023-01-04 at 15 19 53"
src="https://user-images.githubusercontent.com/27995606/210575230-5836c24a-bb0a-4b32-a491-3f2af1edaf41.png">

## Types of changes

<!--- What types of changes does your code introduce? Put an `x` in all
the boxes that apply: -->

- [x] Bug fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)

## Checklist:

<!--- Go over all the following points, and put an `x` in all the boxes
that apply. -->
<!--- If you're unsure about any of these, don't hesitate to ask. We're
here to help! -->

- [x] My code follows the code style of this project.
- [x] My change requires a change to the documentation.
- [ ] I have updated the documentation accordingly.
- [ ] I have added tests to cover my changes.
- [x] All new and existing tests passed.

src/cartesian/Area.tsx
@@ -119,8 +119,12 @@ export class Area extends PureComponent<Props, State> {
   };
 
   static getBaseValue = (props: Props, item: Area, xAxis: Props['xAxis'], yAxis: Props['yAxis']): number => {
-    const { layout } = props;
-    const { baseValue } = item.props;
+    const { layout, baseValue: chartBaseValue } = props;
+    const { baseValue: itemBaseValue } = item.props;
+
+    // The baseValue can be defined both on the AreaChart as well as on the Area.
+    // The value for the item takes precedence.
+    const baseValue = itemBaseValue ?? chartBaseValue;
 
     if (isNumber(baseValue) && typeof baseValue === 'number') {
       return baseValue;